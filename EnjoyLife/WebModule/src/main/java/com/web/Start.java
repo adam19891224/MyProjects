8c4bf90e5e84ce040f4171c5ad2ce3f910af19d5
adam19891224 <273961736@qq.com>
2016-11-08 09:06:06 +0800

    日志配置

--- EnjoyLife/WebModule/src/main/java/com/web/Start.java
+++ EnjoyLife/WebModule/src/main/java/com/web/Start.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 
 import javax.servlet.MultipartConfigElement;
