d8a12e463ae31c736c001524aed8a315af1e5680
adam19891224 <273961736@qq.com>
2017-02-23 18:03:50 +0800

    提交node版

---
+++ EnjoylifePage/log.js
@@ -0,0 +1,29 @@
+const log4js = require('log4js');
+log4js.configure({
+    appenders: [
+        {
+            type: 'console',
+            category: "console"
+        }, //控制台输出
+        {
+            type: "dateFile",
+            filename: "E:/logs/enjoylifepage.log",
+            pattern: "_yyyy-MM-dd",
+            alwaysIncludePattern: false,
+            category: "dateFileLog"
+        } //日期文件格式
+    ],
+    replaceConsole: true,
+    levels:{
+        dateFileLog: "INFO"
+    }
+});
+
+const dateFileLog = log4js.getLogger('dateFileLog');
+
+exports.logger = dateFileLog;
+
+exports.use = function(app) {
+    //页面请求日志,用auto的话,默认级别是WARN
+    app.use(log4js.connectLogger(dateFileLog, {level: 'debug', format: ':method :url'}));
+};
