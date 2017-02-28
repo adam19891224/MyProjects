c966e2ad21e8cfb2cfe432d9eb000a82911e2709
adam19891224 <273961736@qq.com>
2017-02-28 13:51:07 +0800

    删除简单版

--- EnjoylifePage/routes/page.js
+++ EnjoylifePage/routes/page.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const config = require('./config');
-const logger = require('../log').logger;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    logger.info("访问网站。。。");
     res.render('page');
 });
 
