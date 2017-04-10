c0d00e6d04b663dd87f16f3bf75621bb08859155
zhouyuhong <zhouyuhong@zbj.com>
2017-04-10 14:19:04 +0800

    提交

---
+++ AdamStudyProjects/src/main/java/com/adam/Study/Main.java
@@ -0,0 +1,35 @@
+package com.adam.Study;
+
+/**
+ * Created by zbj on 2017/4/10.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        String a = "a";
+        String b = "b";
+        String c = a + b;
+
+        String d = "a" + "b" + "c";
+
+        StringBuilder sb1 = new StringBuilder("aaaaa");
+        String sb2 = "sssss";
+        sb1.append(sb2);
+        System.out.println(c);
+    }
+
+    /**
+     * 上面的反编译后如下：
+     * public static void main(String[] args) {
+             String a = "a";
+             String b = "b";
+             String c = a + b;
+             String d = "abc";
+             StringBuilder sb1 = new StringBuilder("aaaaa");
+             String sb2 = "sssss";
+             sb1.append(sb2);
+             System.out.println(c);
+       }
+     */
+
+}
